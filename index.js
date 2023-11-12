61eb106b5d3b394c3b1f05588895a2b4ec18fa8b
akafc7 <fernandocavanellas6@hotmail.com>
2023-11-12 15:23:44 -0300

    refactor: add route soma

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get('/home', function (req,res) {
-    res.send("Hello World");
+    const soma = 100 + 1;
+
+
+    res.json({soma: "soma"});
 })
 
 app.listen(3000);
