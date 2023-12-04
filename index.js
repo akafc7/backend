2bccb29f5dd60b86ca35f5ce239300c1aa329d75
akafc7 <fernandocavanellas6@hotmail.com>
2023-12-04 18:29:56 -0300

    comparing passwords and logging in

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import connectDatabase from "./src/database/db.js";
-import userRoute from './src/routes/user.route.js';
 import dotenv from "dotenv";
-dotenv.config();
 
+import userRoute from './src/routes/user.route.js';
+import authRoute from './src/routes/auth.route.js';
+
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,5 +14,6 @@ const app = express();
 connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
