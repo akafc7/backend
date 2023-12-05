e3fad8a640bcadc98be653729daf816042e56035
akafc7 <fernandocavanellas6@hotmail.com>
2023-12-05 17:38:01 -0300

    add JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { loginService } from '../services/auth.service.js';
+import { generateToken, loginService } from '../services/auth.service.js';
 
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -7,18 +7,20 @@ const login = async (req, res) => {
     try {
         const user = await loginService(email);
 
-        if(!user){
-            res.status(404).send({message: "User or Password not found"})
+        if (!user) {
+            res.status(404).send({ message: "User or Password not found" })
         }
 
         const passwordIsValid = bcrypt.compareSync(password, user.password);
 
-       if (!passwordIsValid){
-        return res.status(404).send({message: "User or Password not found"})
-       }
+        if (!passwordIsValid) {
+            return res.status(404).send({ message: "User or Password not found" })
+        }
+
+        const token = generateToken(user.id)
 
 
-        res.send("Login ok");
+        res.send({token});
     } catch (err) {
         res.status(500).send(err.message);
     }
